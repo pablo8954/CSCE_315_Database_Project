862fd5ef06f811eb826502c90b5be6df684bb5ae
pablo.say54@gmail.com <pablo.say54@gmail.com>
2020-02-28 15:28:39 -0600

    Add files

---
+++ src/dbSetup.java
@@ -0,0 +1,10 @@
+/*
+Robert Lightfoot
+CSCE 315
+Database setup example
+Purpose: Keeping usernames and password private.
+*/
+public final class dbSetup  {
+  public static final String user = "dbUserName";
+  public static final String pswd = "dbPassword";
+}//end class
