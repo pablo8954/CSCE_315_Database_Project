793839c4d26b8b1da8831ef3110503b99b073653
keeratsingh23 <keeratsingh23@gmail.com>
2020-02-29 14:17:59 -0600

    Attributes.java

---
+++ 315_Phase3_JDBC_GUI/src/application/Attributes.java
@@ -0,0 +1,9 @@
+package application;
+
+public final class Attributes {
+	
+	static final String[] confAttr = {"ConfYearId", "ConfId", "Year", "ConferenceName", 
+			"Subdivision"};
+	static final String[] driveAttr = {};
+
+}
