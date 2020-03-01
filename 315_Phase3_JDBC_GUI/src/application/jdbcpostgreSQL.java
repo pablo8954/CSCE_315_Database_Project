b4147f46f2670c1bdf6d3f2271c5487faf6f42e0
pablo.say54@gmail.com <pablo.say54@gmail.com>
2020-02-29 19:30:19 -0600

    Complete Stadium, Conference Functions

--- 315_Phase3_JDBC_GUI/src/application/jdbcpostgreSQL.java
+++ 315_Phase3_JDBC_GUI/src/application/jdbcpostgreSQL.java
@@ -1,3 +1,5 @@
+package application;
+
 import java.sql.*;
 import javax.swing.JOptionPane;
 
@@ -8,7 +10,7 @@ CSCE 315
 9-25-2019
  */
 public class jdbcpostgreSQL {
-
+	
     public static String stadiumDataFetch(String name, Connection conn) {
         String result_str = "";
         try {
@@ -44,52 +46,40 @@ public class jdbcpostgreSQL {
         }
         return result_str;
     }
+    
+    public static String ConfDataFetch(String name, Connection conn) {
+        String result_str = "";
+        try {
+            // create a statement object
+            Statement stmt = conn.createStatement();
+            // create an SQL statement
+            String sqlStatement = String.format(
+                    "SELECT DISTINCT \"Subdivision\", \"ConferenceName\" FROM \"Conference_Yearwise\" WHERE \"ConfId\" = ( SELECT DISTINCT \"ConfId\" FROM \"Conference_Yearwise\" WHERE \"ConferenceName\" LIKE '%s%%');",
+                    name);
+            // send statement to DBMS
+            ResultSet result = stmt.executeQuery(sqlStatement);
+            while (result.next()) {
+                result_str += "Conference Name: ";
+                result_str += result.getString("ConferenceName");
+                result_str += " | Subdivision: ";
+                result_str += result.getString("Subdivision");
+//                result_str += " | Year: ";
+//                result_str += result.getString("Year");
+                result_str += "\n";
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,
+                    "Error accessing Conference Data. Make sure that the Conference name is correct");
+        }
+        if (result_str == "") {
+            return "Conference Data non existent\n";
+        }
+        return result_str;
+    }
+    
+    
 
     public static void main(String args[]) {
-        // dbSetup my = new dbSetup();
-        // // Building the connection
-        // Connection conn = null;
-        // try {
-        // Class.forName("org.postgresql.Driver");
-        // conn =
-        // DriverManager.getConnection("jdbc:postgresql://csce-315-db.engr.tamu.edu/ace_of_spades",
-        // my.user,
-        // my.pswd);
-        // } catch (Exception e) {
-        // e.printStackTrace();
-        // System.err.println(e.getClass().getName() + ": " + e.getMessage());
-        // System.exit(0);
-        // } // end try catch
-        // JOptionPane.showMessageDialog(null, "Opened database successfully");
-        // String cus_lname = "";
-        // try {
-        // // create a statement object
-        // Statement stmt = conn.createStatement();
-        // // create an SQL statement
-        // String sqlStatement = "SELECT * FROM \"Player\" LIMIT 1;";
-        // // send statement to DBMS
-        // ResultSet result = stmt.executeQuery(sqlStatement);
-
-        // // OUTPUT
-        // JOptionPane.showMessageDialog(null, "Customer Last names from the
-        // Database.");
-        // // System.out.println("______________________________________");
-        // while (result.next()) {
-        // // System.out.println(result.getString("cus_lname"));
-        // cus_lname += result.getString("FirstName") + "\n";
-        // }
-        // } catch (Exception e) {
-        // JOptionPane.showMessageDialog(null, "Error accessing Database.");
-        // }
-        // JOptionPane.showMessageDialog(null, cus_lname);
-        // // closing the connection
-        // try {
-        // conn.close();
-        // JOptionPane.showMessageDialog(null, "Connection Closed.");
-        // } catch (Exception e) {
-        // JOptionPane.showMessageDialog(null, "Connection NOT Closed.");
-        // } // end try catch
-
         dbSetup my = new dbSetup();
         // Building the connection
         Connection conn = null;
@@ -109,6 +99,9 @@ public class jdbcpostgreSQL {
 
         System.out.println(stadiumDataFetch("ASU", conn));
         System.out.println(stadiumDataFetch("Shrey", conn));
+        
+        System.out.println(ConfDataFetch("Big Sky", conn));
+        System.out.println(ConfDataFetch("Football_Sucks", conn));
 
         // END MAIN CODE
         //
@@ -120,5 +113,5 @@ public class jdbcpostgreSQL {
             JOptionPane.showMessageDialog(null, "Connection NOT Closed.");
         }
 
-    }// end main
+    }// end backendmain
 }// end Class
