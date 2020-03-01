eed254228bae1ba09f6a2974716d8c1a5e2309b6
keeratsingh23 <keeratsingh23@gmail.com>
2020-03-01 13:54:13 -0600

    Front end basic

--- 315_Phase3_JDBC_GUI/src/application/dbSetup.java
+++ 315_Phase3_JDBC_GUI/src/application/dbSetup.java
@@ -1,5 +1,7 @@
 package application;
 
-public class dbSetup {
 
+public final class dbSetup  {
+  public static final String user = "pablo_say54";
+  public static final String pswd = "826006015";
 }
