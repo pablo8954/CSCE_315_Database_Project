af6839eecb01ad7b097af3060ea65bcca41b9a44
keeratsingh23 <keeratsingh23@gmail.com>
2020-03-01 14:20:56 -0600

    Front end basic complete

--- 315_Phase3_JDBC_GUI/src/application/GUIMain.java
+++ 315_Phase3_JDBC_GUI/src/application/GUIMain.java
@@ -8,8 +8,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-	
+public class GUIMain extends Application {
+
 	@Override
 	public void start(Stage primaryStage) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("Window.fxml"));
--- 315_Phase3_JDBC_GUI/src/application/dbSetup.java
+++ 315_Phase3_JDBC_GUI/src/application/dbSetup.java
@@ -1,7 +1,5 @@
 package application;
 
+public class dbSetup {
 
-public final class dbSetup  {
-  public static final String user = "pablo_say54";
-  public static final String pswd = "826006015";
 }
