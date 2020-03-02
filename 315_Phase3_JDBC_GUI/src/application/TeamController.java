665b99e9d5101788bc389b01e30402d51b9db9bd
keeratsingh23 <keeratsingh23@gmail.com>
2020-03-01 22:52:00 -0600

    Team pop up edited for radio

--- 315_Phase3_JDBC_GUI/src/application/TeamController.java
+++ 315_Phase3_JDBC_GUI/src/application/TeamController.java
@@ -4,11 +4,18 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class TeamController {
 	
+	@FXML
+	RadioButton GeneralRadio;
+	@FXML
+	RadioButton GameRadio;
+	@FXML
+	RadioButton PlayRadio;
 	@FXML 
 	TextField TeamNameText;
 	String teamName;
